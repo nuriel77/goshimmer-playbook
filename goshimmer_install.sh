5e4d392c777aa7d5c6fee076f5eef796630d294f
Nuriel Shem-Tov <nurielst@hotmail.com>
2019-08-24 09:18:52 +0200

    Add lsb-release to dependencies apt

--- goshimmer_install.sh
+++ goshimmer_install.sh
@@ -184,7 +184,7 @@ function init_ubuntu(){
     apt-add-repository ppa:ansible/ansible -y
     add-apt-repository universe -y
     apt-get update -y
-    apt-get install ansible git expect-dev tcl libcrack2 cracklib-runtime whiptail -y
+    apt-get install ansible git expect-dev tcl libcrack2 cracklib-runtime whiptail lsb-release -y
 }
 
 function init_debian(){
@@ -205,7 +205,7 @@ function init_debian(){
     apt-get install dirmngr --install-recommends -y
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
     apt-get update -y
-    apt-get install ansible git expect-dev tcl libcrack2 cracklib-runtime whiptail -y
+    apt-get install ansible git expect-dev tcl libcrack2 cracklib-runtime whiptail lsb-release -y
 }
 
 function inform_reboot() {
