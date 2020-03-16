7ae98c48bc0b4a7b7e490fa73b1bf1c05f7a7242
Nuriel Shem-Tov <nurielst@hotmail.com>
2020-03-16 22:02:27 +0100

    Add required uid tag

--- custom_updates/0.1.4_updates.sh
+++ custom_updates/0.1.4_updates.sh
@@ -2,5 +2,5 @@
 
 if /usr/bin/docker images "iotaledger/goshimmer:v0.1.3" | grep -q v0.1.3
 then
-    cd /opt/goshimmer-playbook && git pull && ansible-playbook -i inventory site.yml -v --tags=goshimmer_service_file
+    cd /opt/goshimmer-playbook && git pull && ansible-playbook -i inventory site.yml -v --tags=goshimmer_service_file,get_goshimmer_uid
 fi
