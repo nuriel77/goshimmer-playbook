fadb94f54dc37a5c4e7258245c7ccf77260a1421
Nuriel Shem-Tov <nurielst@hotmail.com>
2020-07-04 16:08:13 +0200

    Sort pip 2.7 compatibility

--- goshimmer_install.sh
+++ goshimmer_install.sh
@@ -186,6 +186,7 @@ function init_centos_7(){
       cracklib\
       newt\
       python3\
+      python-pip\
       python-pip3
     if [ -e /usr/bin/pip ]; then
         /usr/bin/pip install jmespath
